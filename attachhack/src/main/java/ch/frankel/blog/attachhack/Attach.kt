e75921773668dd301a6c41cfd5e7df91a472e31b
Nicolas Frankel <nicolas@frankel.ch>
2021-11-08 19:55:46 +0100

    Automatically find PID instead of passing it on the command-line

--- attachhack/src/main/java/ch/frankel/blog/attachhack/Attach.kt
+++ attachhack/src/main/java/ch/frankel/blog/attachhack/Attach.kt
@@ -1,13 +1,34 @@
 package ch.frankel.blog.attachhack
 
 import com.sun.tools.attach.VirtualMachine
+import java.io.Closeable
+import java.nio.charset.Charset
+
 
 fun main(args: Array<String>) {
-    with(VirtualMachine.attach(args[0])) {
-        try {
-            loadAgent(args[1])
-        } finally {
-            detach()
+    Runtime.getRuntime()
+        .exec("jps")
+        .inputStream
+        .bufferedReader(Charset.forName("UTF-8"))
+        .lines()
+        .map { it.split(" ").toTypedArray() }
+        .filter { it.size > 1 && it[1].endsWith("BusinessApplicationKt") }
+        .map { it[0] }
+        .forEach { pid ->
+            VirtualMachine.attach(pid).toCloseable().use {
+                it.loadAgent(args[0])
+            }
         }
+}
+
+fun VirtualMachine.toCloseable() = CloseableVirtualMachine(this)
+
+class CloseableVirtualMachine(private val vm: VirtualMachine) : Closeable {
+    fun loadAgent(arg: String) {
+        vm.loadAgent(arg)
+    }
+
+    override fun close() {
+        vm.detach()
     }
 }
